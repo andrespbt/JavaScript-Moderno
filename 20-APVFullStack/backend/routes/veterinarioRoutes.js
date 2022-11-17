7c1acb68c3ad9eb19d4b43be067a999d8f26bfcf
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-11-17 13:32:31 -0300

    Cambiar password y validacion finalizados

--- 20-APVFullStack/backend/routes/veterinarioRoutes.js
+++ 20-APVFullStack/backend/routes/veterinarioRoutes.js
@@ -1,14 +1,29 @@
 import express from 'express';
-import { registrar, perfil, confirmar, autenticar } from '../controlers/veterinarioControler.js';
+import {
+  registrar,
+  perfil,
+  confirmar,
+  autenticar,
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword
+} from '../controlers/veterinarioControler.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+// Area publica
 router.post('/', registrar);
 
 router.get('/confirmar/:token', confirmar);
 
 router.post('/login', autenticar);
+
+router.post('/olvide-password', olvidePassword);
+
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+// Area privada
 
 router.get('/perfil', checkAuth, perfil);
 

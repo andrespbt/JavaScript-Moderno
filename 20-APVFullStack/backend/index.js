70fd3e12b96e39f9017570b0d6d3a609648ea0ab
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-12-16 17:59:19 -0300

    Vistas registrar, olivde password, login agregadas

--- 20-APVFullStack/backend/index.js
+++ 20-APVFullStack/backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import conectarDB from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js';
 import pacienteRoutes from './routes/pacienteRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(express.json());
 dotenv.config();
 
 conectarDB();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  next();
+});
 
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
---
+++ 20-APVFullStack/frontend/src/paginas/ConfirmarCuenta.jsx
@@ -0,0 +1,9 @@
+const ConfirmarCuenta = () => {
+    return (
+      <>
+          <h1>Desde confirmarCuenta</h1>
+      </>
+    )
+  }
+  
+  export default ConfirmarCuenta

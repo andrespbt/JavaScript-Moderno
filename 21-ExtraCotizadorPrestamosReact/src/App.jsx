35764242db061c54eb36dc8018723b202773dbf0
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-12-25 19:51:56 -0300

    Proyecto cotizador prestamos react finalizado

--- 21-ExtraCotizadorPrestamosReact/src/App.jsx
+++ 21-ExtraCotizadorPrestamosReact/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Button from './components/Button';
-import { formatearDinero, handleKeyDown } from './helpers/index'
-function App(props) {
+import { formatearDinero, calcularTotalPagar } from './helpers/index'
+function App() {
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total,setTotal] = useState(0);
+
+  useEffect(() =>{
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses)
+    setTotal(resultadoTotalPagar);
+
+  }, [cantidad, meses])
+
+
   const MIN = 0;
   const MAX = 20000;
   const STEP = 100;
@@ -55,6 +65,30 @@ function App(props) {
 
        <p className='text-center my-10 text-5xl font-extrabold text-indigo-500'>{formatearDinero(cantidad)}
        </p>
+
+       <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span className='text-indigo-600'>Plazo</span> a pagar
+       </h2>
+
+       <select 
+        className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+        value={meses}
+        onChange={e => setMeses(+e.target.value)}
+        >
+          <option value="6">6 meses</option>
+          <option value="12">12 meses</option>
+          <option value="24">24 meses</option>
+       </select>
+
+       <div className='my-5 space-y-3 bg-gray-50 p-5'>
+          <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span className='text-indigo-600'>de pagos</span>
+          </h2>
+          
+          <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+          <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total)} Total a pagar</p>
+          <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total / meses)} Pago mensual</p>
+       </div>
     </div>
   )
 }

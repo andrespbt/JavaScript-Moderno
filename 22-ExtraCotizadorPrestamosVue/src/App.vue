516bd282d07f29c7727bd943f8e913b1a8aeb611
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-12-26 17:29:33 -0300

    Cotizador prestamos vue finalizado

--- 22-ExtraCotizadorPrestamosVue/src/App.vue
+++ 22-ExtraCotizadorPrestamosVue/src/App.vue
@@ -1,29 +1,106 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, computed, watch} from 'vue';
+  import Header from './components/Header.vue';
+  import Button from './components/Button.vue';
+
+  import {calcularTotalPagar} from './helpers'
+
+  const cantidad = ref(10000);
+  const meses = ref(6);
+  const total = ref(0);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+  
+  const formatearDinero = (valor) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    return formatter.format(valor)
+  }
+  
+  watch([cantidad, meses],  () => {
+    total.value = calcularTotalPagar(cantidad.value, meses.value)
+  });
+
+  const pagoMensual = computed(() => {
+    return total.value / meses.value
+  })
+
+  const handleChangeDecremento = () => {
+    if(cantidad.value > MIN){
+      cantidad.value = cantidad.value - STEP;
+    }
+    
+  }
+
+  const handleChangeIncremento = () => {
+    if(cantidad.value < MAX){
+      cantidad.value = cantidad.value + STEP;
+    }
+    
+  }
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header/>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      <div class="flex justify-between mt-10">
+        <Button 
+          :operador = "'-'"
+          @fn = "handleChangeDecremento"
+        />
+        <Button
+          :operador = "'+'"
+          @fn = "handleChangeIncremento"
+        />
+      </div>
+
+      <div class="my-5">
+        <input 
+        type="range"
+        class="w-full bg-gray-500 accent-lime-500 hover:accent-lime-600"
+        :min= 'MIN'
+        :max= 'MAX'
+        :step= 'STEP'
+        v-model.number = "cantidad"
+        />
+
+        <p 
+          class="text-center my-10 text-5xl font-extrabold text-indigo-600"
+          > {{ formatearDinero(cantidad) }}
+        </p>
+
+        <h2 
+          class="text-xl font-extrabold text-gray-500 text-center"
+          >Elige un <span class="text-indigo-600"> plazo </span> a pagar
+        </h2>
+
+        <select 
+          class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 mt-5"
+          :value="meses"
+          v-model.number = "meses"
+        >
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
+        </select>
+      </div>
+
+      <div v-if="total > 0" class="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span class="text-indigo-600"> de pagos</span>
+        </h2>
+        <p class="text-xl text-gray-500 text-center font-bold">{{ meses }}  Meses</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Total a pagar: {{ formatearDinero(total) }}</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Mensuales: {{ formatearDinero(pagoMensual) }}</p>
+      </div>
+
+      <p v-else class="text-center">Añade una cantidad y el plazo a pagar</p>
+  </div>
+</template>
